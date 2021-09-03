2c9fdda747b5b10c4b8bd7471f43fda53d8df77c
Filip <fili4252@stud.kea.dk>
2021-09-03 15:17:55 +0200

    made each div appear with a height from number array

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict";
 
-const bar = document.querySelector(".bar");
 // const queueSize = numberOfCustomers();
 let iterator;
+let bar;
+
 let valueAtIndex;
 let maxIterations;
 const numbersArr = [5, 2, 3, 4, 13, 30, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 23, 20, 30, 11, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 3, 4];
@@ -16,24 +17,22 @@ init();
 
 function init() {
   console.log("init()");
-  iterator = 0;
+  iterator = 1;
   maxIterations = numbersArr.length;
+  bar = document.querySelector("#bar" + iterator);
+  console.log(bar);
   loopNumbersArr();
 }
 
 function loopNumbersArr() {
   console.log("loopNumbersArr()");
-  valueAtIndex = numbersArr[iterator];
+  valueAtIndex = numbersArr[iterator - 1];
   iterator++;
+  bar = document.querySelector("#bar" + iterator);
   changeHeight();
-  // createBar();
   if (iterator < maxIterations) {
-    setTimeout(loopNumbersArr, 100);
+    setTimeout(loopNumbersArr, 1000);
   }
-}
-
-function createBar() {
-  console.log("createBar()");
 }
 
 function changeHeight() {
