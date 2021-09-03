e15fe4765be696ca9ab7255434a223bbb771f9a9
Filip <fili4252@stud.kea.dk>
2021-09-03 14:28:59 +0200

    made a loop function that changes heuight of the first bar

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 "use strict";
 
 const bar = document.querySelector(".bar");
-const queueSize = numberOfCustomers();
-
-function numberOfCustomers() {
-  return Math.floor(Math.random() * 32);
-}
-
+// const queueSize = numberOfCustomers();
+let iterator;
+let valueAtIndex;
+let maxIterations;
 const numbersArr = [5, 2, 3, 4, 13, 30, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 23, 20, 30, 11, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 3, 4];
-console.log(numbersArr.length);
-const iterator = numbersArr.values();
 
-for (const value of iterator) {
-  console.log(value);
-  setTimeout(1000);
+//calculate random numbers between 0 and 32
+// function numberOfCustomers() {
+//   return Math.floor(Math.random() * 32);
+// }
+
+init();
+
+function init() {
+  console.log("init()");
+  iterator = 0;
+  maxIterations = numbersArr.length;
+  loopNumbersArr();
 }
 
-// changeHeight();
+function loopNumbersArr() {
+  console.log("loopNumbersArr()");
+  valueAtIndex = numbersArr[iterator];
+  iterator++;
+  changeHeight();
+  // createBar();
+  if (iterator < maxIterations) {
+    setTimeout(loopNumbersArr, 100);
+  }
+}
 
-// function changeHeight() {
-//   bar.style.height = number + "rem";
-//   setTimeout(changeHeight, 1000);
-// }
+function createBar() {
+  console.log("createBar()");
+}
+
+function changeHeight() {
+  bar.style.height = valueAtIndex + "rem";
+}
