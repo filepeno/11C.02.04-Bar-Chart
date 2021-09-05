29f841cdee91d196cfcb955449a64e2fd4de4b08
Filip <fili4252@stud.kea.dk>
2021-09-05 16:39:30 +0200

    made the bars work like in assignment desc

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 "use strict";
-
-// const queueSize = numberOfCustomers();
 let iterator;
-let bar;
+let bar = document.querySelector("#bar" + iterator);
 
 let valueAtIndex;
 let maxIterations;
 const numbersArr = [5, 2, 3, 4, 13, 30, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 23, 20, 30, 11, 32, 12, 19, 27, 31, 27, 22, 10, 11, 14, 3, 26, 28, 8, 3, 4];
-
-//calculate random numbers between 0 and 32
-// function numberOfCustomers() {
-//   return Math.floor(Math.random() * 32);
-// }
+let queueSize;
 
 init();
 
@@ -19,8 +13,6 @@ function init() {
   console.log("init()");
   iterator = 1;
   maxIterations = numbersArr.length;
-  bar = document.querySelector("#bar" + iterator);
-  console.log(bar);
   loopNumbersArr();
 }
 
@@ -31,10 +23,21 @@ function loopNumbersArr() {
   bar = document.querySelector("#bar" + iterator);
   changeHeight();
   if (iterator < maxIterations) {
-    setTimeout(loopNumbersArr, 1000);
+    loopNumbersArr();
+  } else {
+    //delete first value in array
+    numbersArr.shift([0]);
+    //calculate random numbers between 0 and 32
+    queueSize = Math.floor(Math.random() * 32);
+    console.log(queueSize);
+    //push the new value into array
+    numbersArr.push(queueSize);
+    console.log(numbersArr);
+    setTimeout(init, 1000);
   }
 }
 
 function changeHeight() {
+  console.log("changeHeight()");
   bar.style.height = valueAtIndex + "rem";
 }
